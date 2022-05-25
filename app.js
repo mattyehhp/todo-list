5743f423ae5a151d655ea8ec97f9a685e174896f
mattyehhp <matt26520@gmail.com>
2022-05-26 04:20:42 +0800

    Initialize Project

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const port = 3000
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log(`Express is listening on localhost:${port}`)
+})
