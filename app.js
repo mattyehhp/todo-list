e81ca565f813d7dc2a910576ae3c6e05768eec8b
mattyehhp <matt26520@gmail.com>
2022-05-28 18:11:48 +0800

    add create and delete functions

--- app.js
+++ app.js
@@ -1,13 +1,82 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Todo = require('./models/todo')
+const bodyParser = require('body-parser')
+
 const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+    console.log('mongodb error')
+})
+
+db.once('open', () => {
+    console.log('mongodb connected!')
+})
+
 const exphbs = require('express-handlebars')
+const todo = require('./models/todo')
 const port = 3000
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-    res.render('index')
+    Todo.find()
+        .lean()
+        .then(todos => res.render('index', { todos }))
+        .catch(error => console.error(error))
+})
+
+app.get('/todos/new', (req, res) => {
+    return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+    const name = req.body.name
+    return Todo.create({ name })
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error)) 
+})
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id
+    return Todo.findById(id)
+        .lean()
+        .then((todo) => res.render('detail', { todo }))
+        .catch(error => console.log(error))
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+    const id = req.params.id
+    return Todo.findById(id)
+        .lean()
+        .then((todo) => res.render('edit', { todo }))
+        .catch((error) => console.log(error))
+})
+
+app.post('/todos/:id/edit', (req, res) => {
+    const id = req.params.id
+    const name = req.body.name
+    return Todo.findById(id) 
+        .then((todo) => {
+            todo.name = name
+            return todo.save()
+        })
+        .then(() => res.redirect(`/todos/${id}`))
+        .catch(error => console.log(error))
+})
+
+app.post('/todos/:id/delete', (req, res) => {
+    const id = req.params.id
+    return Todo.findById(id)
+        .then((todo) => todo.remove())
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
